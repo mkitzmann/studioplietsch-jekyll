9da590822e1bdf2417734e2a05c2a494b8575898
moritzkitzmann <mkitzmann@gmail.com>
2018-08-07 09:09:01 +0200

    cms preview

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!")
+import React from "react";
+import CMS from "netlify-cms";
+
+import ProjectPreview from "./cms-preview-templates/project";
+
+// Example of creating a custom color widget
+class ColorControl extends React.Component {
+  render() {
+    return <input
+        style={{height: "80px"}}
+        type="color"
+        value={this.props.value}
+        onInput={(e) => this.props.onChange(e.target.value)}
+    />;
+  }
+}
+
+CMS.registerPreviewStyle("/css/app.css");
+CMS.registerPreviewTemplate("projects", ProjectPreview);
