095547959bfe68ab2d4648ac314a254c675c827c
moritzkitzmann <mkitzmann@gmail.com>
2018-08-07 09:40:12 +0200

    project preview

--- webpack/cms-preview-templates/project.js
+++ webpack/cms-preview-templates/project.js
@@ -2,13 +2,34 @@ import React from "react";
 
 export default class ProjectPreview extends React.Component {
   render() {
-    const {entry, widgetFor, getAsset} = this.props;
+    const {entry, widgetFor, widgetsFor , getAsset} = this.props;
 
     let thumbnail = entry.getIn(["data", "thumbnail"]);
 
     return <section id="body">
       <div class="container">
+
       
+
+      {widgetsFor('sections').map((section) =>
+        <div>
+          <div className="project-images">
+          {section.getIn(['data','images']).map((image) =>
+            <div className="image-wrapper">
+              <div>
+                {console.log(getAsset(image.get('image')))}
+              </div>
+              <img src={getAsset(image.get('image'))} />
+            </div>
+          )}
+          </div>
+
+          <div>
+          {section.getIn(['widgets','text'])}
+          </div>
+        </div>
+      )}
+{/*       
         {(entry.getIn(["data", "sections"]) || []).map((section, i) => 
         <div>
           <div className="project-images" key={i}>
@@ -24,7 +45,7 @@ export default class ProjectPreview extends React.Component {
           <p>{ section.get("text") }</p>
                   
 
-        </div>)}
+        </div>)} */}
 
       </div>
     </section>;
